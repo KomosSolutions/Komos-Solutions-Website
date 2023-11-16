7c471dc734106ccc5a55ee4151b0a23cb1aa0c90
Damian <fairholmed@gmail.com>
2023-11-16 17:55:19 +0000

    fixing site to work with git pages

--- docs/thankyou.php
+++ docs/thankyou.php
@@ -7,22 +7,22 @@ $company = $_REQUEST['company'];
 $comments = $_REQUEST['message'];
 $type = $_REQUEST['type'];
 
-$spam= eregi('href', $comments);
-$spamb = eregi('url', $comments);
+$spam= preg_match('/href/', $comments);
+$spamb = preg_match('/url/', $comments);
 $spamc = $spam+ $spamb;
 
 if ($spamc == 0){
-$headers .= "From: donotreply@komossolutions.com\n";  // your email client will show the person's email address like normal
+$headers = "From: donotreply@komossolutions.com\n";
 $headers .= "Content-Type: text/plain; charset=iso-8859-1\n"; // sets the mime type
-$recipient = "hello@komossolutions.com"; // enter YOUR email address here
-$subject = "Komos Contact Form"; // this is the subject of the email
+$recipient = "hello@komossolutions.com";
+$subject = "Komos Contact Form"; // subject of the email
 
 $msg = "Contact Type: " .$type. "\n Name: ".$name. "\n Telephone Number: ".$tel. "\n Email: ".$email."\n Company: ".$company."\n Comments: ".$comments."\n\n Spam: ".$spamc."\n Powered By Advanta Internet\n";
 
-mail($recipient, $subject, $msg, $headers); // the mail() function sends the message to you
+mail($recipient, $subject, $msg, $headers);
 }
 
 
 //Once the data is entered, redirect the user to give them visual confirmation
   echo "<script>document.location.href='thankyou.html'</script>";
-?>
+
